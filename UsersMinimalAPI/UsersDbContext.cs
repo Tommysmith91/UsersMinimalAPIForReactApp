4a61f351901a24ea2929e42e0d417eecb9bb58e5
tommy.smith <tommy.smith@cloudcommercegroup.com>
2024-03-25 16:00:34 +0000

    Initial User DBcreation followed by writing password hasher service

---
+++ UsersMinimalAPI/UsersDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersMinimalAPI.Entities;
+
+namespace UsersMinimalAPI
+{
+    public class UsersDbContext : DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get;set; }
+
+    }
+}
